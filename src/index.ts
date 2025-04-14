9a276837c86ede3e3733844811a2a5534d317f50
Vyacheslav Volodin <vyacheslav.volodin@my.games>
2025-04-14 14:16:18 +0300

    fix index

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ import NeoCard from "./components/card/vue/NeoCard.vue";
 import NeoDialog from "./components/dialog/vue/NeoDialog.vue";
 import NeoInput from "./components/input/vue/NeoInput.vue";
 import NeoTag from "./components/tag/vue/NeoTag.vue";
-// Import other components like NeoCard, NeoInput, etc.
-// import NeoCard from './components/NeoCard.vue';
 
 // Import the main CSS file if you want it bundled/extracted automatically
 // Or instruct users to import it separately.
