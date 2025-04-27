47784ae5895934d87c95ea3b99059203bf8d823e
Vyacheslav Volodin <vyacheslav.volodin@my.games>
2025-04-27 16:08:06 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import type { NeoButtonProps } from "./components/button";
 import type { NeoCardProps } from "./components/card";
 import type { NeoDialogProps } from "./components/dialog";
 import type { NeoInputProps } from "./components/input";
+import type { NeoTab, NeoTabsProps } from "./components/tabs";
 import type { NeoTagProps } from "./components/tag";
 import { NeoButton } from "./components/button";
 import { NeoCard } from "./components/card";
@@ -13,7 +14,7 @@ import { NeoTag } from "./components/tag";
 import "./assets/styles/theme.css";
 
 export { NeoButton, NeoCard, NeoDialog, NeoInput, NeoTabs, NeoTag };
-export type { NeoButtonProps, NeoCardProps, NeoDialogProps, NeoInputProps, NeoTagProps };
+export type { NeoButtonProps, NeoCardProps, NeoDialogProps, NeoInputProps, NeoTab, NeoTabsProps, NeoTagProps };
 export default {
   install(app: import("vue").App) {
     app.component("NeoButton", NeoButton);
